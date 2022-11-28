ea69089f64a21cd8f61e6cb48cff88eb0db5e079
liujie <tiandiweb@qq.com>
2022-11-28 11:29:23 +0800

    tiem

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
  * @Author: liujie tiandiweb@qq.com
  * @Date: 2022-11-28 11:00:25
  * @LastEditors: liujie tiandiweb@qq.com
- * @LastEditTime: 2022-11-28 11:01:32
+ * @LastEditTime: 2022-11-28 11:18:51
  * @FilePath: \mk-webd:\work\svgdemo\css-scale\dist\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
